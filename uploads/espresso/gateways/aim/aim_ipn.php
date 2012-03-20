e50b0b795d738531396e178a30e0bbc644efd4b5
Matt Stratton <matt.stratton@gmail.com>
2012-03-20 06:59:01 -0500

    Updates for the EE plugin

--- uploads/espresso/gateways/aim/aim_ipn.php
+++ uploads/espresso/gateways/aim/aim_ipn.php
@@ -1,97 +1,1 @@
-<?php
-global $wpdb, $org_options;
-echo '<!--Event Espresso Authorize.net AIM Gateway Version ' . $authnet_aim_gateway_version . '-->';
-
-require_once 'AuthorizeNet.php';
-
-$authnet_aim_settings = get_option('event_espresso_authnet_aim_settings');
-$authnet_aim_login_id = $authnet_aim_settings['authnet_aim_login_id'];
-$authnet_aim_transaction_key = $authnet_aim_settings['authnet_aim_transaction_key'];
-
-// Enable test mode if needed
-//4007000000027  <-- test successful visa
-//4222222222222  <-- test failure card number
-if ($authnet_aim_settings['use_sandbox'] == '1'){
-	define("AUTHORIZENET_SANDBOX", true);
-	define("AUTHORIZENET_LOG_FILE", true);
-} else define("AUTHORIZENET_SANDBOX", false);
-
-//start transaction
-$transaction = new AuthorizeNetAIM($authnet_aim_login_id, $authnet_aim_transaction_key);
-$transaction->amount = $_POST['amount'];
-$transaction->card_num = $_POST['card_num'];
-$transaction->exp_date = $_POST['exp_date'];
-$transaction->first_name = $_POST['first_name'];
-$transaction->last_name = $_POST['last_name'];
-$transaction->email = $_POST['email'];
-$transaction->address = $_POST['address'];
-$transaction->city = $_POST['city'];
-$transaction->state = $_POST['state'];
-$transaction->zip = $_POST['zip'];
-$transaction->cust_id = $_POST['x_cust_id'];
-$transaction->invoice_num = $_POST['invoice_num'];
-$transaction->description = $_POST['class_desc'];
-
-//Capture response
-$response = $transaction->authorizeAndCapture();
-
-$txn_id = $response->transaction_id;
-$attendee_id = $response->customer_id;
-$txn_type = $response->transaction_type;
-$payment_date = date("d-m-Y");
-
-$sql = "SELECT * FROM ". EVENTS_ATTENDEE_TABLE . " WHERE registration_id='" . espresso_registration_id($attendee_id) . "' ";
-$sql .= $id ==''?'':" AND id= '".$id."' ";
-$sql .= " ORDER BY id LIMIT 0,1";
-
-$attendees = $wpdb->get_results($sql);
-foreach ($attendees as $attendee){
-	$attendee_id = $attendee->id;
-	$att_registration_id = $attendee->registration_id;
-	$registration_id = $att_registration_id;
-	$lname = $attendee->lname;
-	$fname = $attendee->fname;
-	$email = $attendee->email;
-	$amount_pd = $attendee->amount_pd;
-	$event_id = $attendee->event_id;
-}
-
-$events = $wpdb->get_results( "SELECT * FROM " . EVENTS_DETAIL_TABLE . " WHERE id='" . $event_id . "'" );
-foreach ( $events as $event ) {
-	$event_id = $event->id;
-	$event_name = $event->event_name;
-	$event_desc = $event->event_desc;
-	$event_description = $event->event_desc;
-	$event_identifier = $event->event_identifier;
-	$cost = $event->event_cost;
-	$event_cost = $event->event_cost;
-	$active = $event->is_active;
-}
-//Build links
-$event_url = espresso_reg_url($event_id);
-$event_link = '<a href="' . $event_url . '">' . $event_name . '</a>';
-
-if ($response->approved) {
-	$payment_status = 'Completed';
-?>
-	<h2><?php _e('Thank You!','event_espresso'); ?></h2>
-	<p><?php _e('Your transaction has been processed.','event_espresso'); ?></p>
-	<p><?php __('Transaction ID:','event_espresso') . $response->transaction_id; ?></p>
-<?php
-} else {
-	print $response->error_message;
-	$payment_status = 'Payment Declined';
-	$payment_failed = true;
-}
-
-$sql = "UPDATE ". EVENTS_ATTENDEE_TABLE . " SET payment_status = '" . $payment_status . "', txn_type = '" . $txn_type . "', txn_id = '" . $txn_id . "', payment_date ='" . $payment_date . "', transaction_details = '" . serialize($response) . "'  WHERE registration_id ='" . espresso_registration_id($attendee_id) . "'";
-$wpdb->query($sql);
-
-//Debug
-//print_r($response);
-//echo $att_registration_id;
-
-//If the payment fails, then we display the payment page
-if ($payment_failed ==true){
-	echo event_espresso_pay($att_registration_id);
-}
+<?phpfunction espresso_transactions_aim_get_attendee_id($attendee_id) {	if (!empty($_REQUEST['x_cust_id'])) {		$attendee_id = $_REQUEST['x_cust_id'];	}	return $attendee_id;}add_filter('filter_hook_espresso_transactions_get_attendee_id', 'espresso_transactions_aim_get_attendee_id');function espresso_process_aim($payment_data) {	extract($payment_data);	global $wpdb, $org_options;	require_once 'AuthorizeNet.php';	$authnet_aim_settings = get_option('event_espresso_authnet_aim_settings');	$authnet_aim_login_id = $authnet_aim_settings['authnet_aim_login_id'];	$authnet_aim_transaction_key = $authnet_aim_settings['authnet_aim_transaction_key'];// Enable test mode if needed//4007000000027  <-- test successful visa//4222222222222  <-- test failure card number	if ($authnet_aim_settings['use_sandbox']) {		define("AUTHORIZENET_SANDBOX", true);		define("AUTHORIZENET_LOG_FILE", true);	} else {		define("AUTHORIZENET_SANDBOX", false);	}//start transaction	$transaction = new AuthorizeNetAIM($authnet_aim_login_id, $authnet_aim_transaction_key);	echo '<!--Event Espresso Authorize.net AIM Gateway Version ' . $transaction->gateway_version . '-->';	$transaction->amount = $_POST['amount'];	$transaction->card_num = $_POST['card_num'];	$transaction->exp_date = $_POST['exp_date'];	$transaction->card_code = $_POST['ccv_code'];	$transaction->first_name = $_POST['first_name'];	$transaction->last_name = $_POST['last_name'];	$transaction->email = $_POST['email'];	$transaction->address = $_POST['address'];	$transaction->city = $_POST['city'];	$transaction->state = $_POST['state'];	$transaction->zip = $_POST['zip'];	$transaction->cust_id = $_POST['x_cust_id'];	$transaction->invoice_num = $_POST['invoice_num'];		$transaction->description = $_POST['class_desc'];	if ($authnet_aim_settings['test_transactions']) {		$transaction->test_request = "true";	}	$payment_data['txn_type'] = 'authorize.net AIM';	$payment_data['payment_status'] = 'Incomplete';	$payment_data['txn_id'] = 0;	$payment_data['txn_details'] = 'No response from authorize.net';	$payment_data = apply_filters('filter_hook_espresso_prepare_event_link', $payment_data);	$payment_data = apply_filters('filter_hook_espresso_get_total_cost', $payment_data);//Capture response	$response = $transaction->authorizeAndCapture();	if (!empty($response)) {		$payment_data['txn_id'] = $response->transaction_id;		$payment_data['txn_details'] = serialize($response);		if ($response->approved) {			$payment_data['payment_status'] = 'Completed';			?>			<h2><?php _e('Thank You!', 'event_espresso'); ?></h2>			<p><?php _e('Your transaction has been processed.', 'event_espresso'); ?></p>			<p><?php __('Transaction ID:', 'event_espresso') . $response->transaction_id; ?></p>			<?php		} else {			print $response->error_message;			$payment_data['payment_status'] = 'Payment Declined';		}	} else {		?>		<p><?php _e('There was no response from Authorize.net.', 'event_espresso'); ?></p>		<?php	}	$payment_data = apply_filters('filter_hook_espresso_update_attendee_payment_data_in_db', $payment_data);	do_action('action_hook_espresso_email_after_payment', $payment_data);	return $payment_data;}add_filter('filter_hook_espresso_thank_you_get_payment_data', 'espresso_process_aim');
--- uploads/espresso/templates/event_list_display.php
+++ uploads/espresso/templates/event_list_display.php
@@ -1,90 +1,146 @@
-<?php 
+<?php
 //This is the event list template page.
 //This is a template file for displaying an event lsit on a page.
 //There should be a copy of this file in your wp-content/uploads/espresso/ folder.
 /*
-* use the following shortcodes in a page or post:
-* [EVENT_LIST]
-* [EVENT_LIST limit=1]
-* [EVENT_LIST show_expired=true]
-* [EVENT_LIST show_deleted=true]
-* [EVENT_LIST show_secondary=true]
-* [EVENT_LIST show_recurrence=true]
-* [EVENT_LIST category_identifier=your_category_identifier]
-*
-* Example:
-* [EVENT_LIST limit=5 show_recurrence=true category_identifier=your_category_identifier]
-*
-*/
-$first_event_instance = $events_group[0];
-$first_event_excerpt = array_shift(explode('<!--more-->', html_entity_decode($first_event_instance['event_desc'])));
+ * use the following shortcodes in a page or post:
+ * [EVENT_LIST]
+ * [EVENT_LIST limit=1]
+ * [EVENT_LIST css_class=my-custom-class]
+ * [EVENT_LIST show_expired=true]
+ * [EVENT_LIST show_deleted=true]
+ * [EVENT_LIST show_secondary=false]
+ * [EVENT_LIST show_recurrence=true]
+ * [EVENT_LIST category_identifier=your_category_identifier]
+ *
+ * Example:
+ * [EVENT_LIST limit=5 show_recurrence=true category_identifier=your_category_identifier]
+ *
+ */
 
+//Print out the array of event status options
+//print_r (event_espresso_get_is_active($event_id));
+//Here we can create messages based on the event status. These variables can be echoed anywhere on the page to display your status message.
+$status = event_espresso_get_is_active(0,$event_meta);
+$status_display = ' - ' . $status['display_custom'];
+$status_display_ongoing = $status['status'] == 'ONGOING' ? ' - ' . $status['display_custom'] : '';
+$status_display_deleted = $status['status'] == 'DELETED' ? ' - ' . $status['display_custom'] : '';
+$status_display_secondary = $status['status'] == 'SECONDARY' ? ' - ' . $status['display_custom'] : ''; //Waitlist event
+$status_display_draft = $status['status'] == 'DRAFT' ? ' - ' . $status['display_custom'] : '';
+$status_display_pending = $status['status'] == 'PENDING' ? ' - ' . $status['display_custom'] : '';
+$status_display_denied = $status['status'] == 'DENIED' ? ' - ' . $status['display_custom'] : '';
+$status_display_expired = $status['status'] == 'EXPIRED' ? ' - ' . $status['display_custom'] : '';
+$status_display_reg_closed = $status['status'] == 'REGISTRATION_CLOSED' ? ' - ' . $status['display_custom'] : '';
+$status_display_not_open = $status['status'] == 'REGISTRATION_NOT_OPEN' ? ' - ' . $status['display_custom'] : '';
+$status_display_open = $status['status'] == 'REGISTRATION_OPEN' ? ' - ' . $status['display_custom'] : '';
+
+//You can also display a custom message. For example, this is a custom registration not open message:
+$status_display_custom_closed = $status['status'] == 'REGISTRATION_CLOSED' ? ' - <span class="espresso_closed">' . __('Regsitration is Closed', 'event_espresso') . '</span>' : '';
+global $this_event_id;
+$this_event_id = $event_id;
 ?>
-<div id="event_data-<?php echo $first_event_instance['event_id']?>" class="event_data subpage_excerpt r">
-    
-    <h3 id="event_title-<?php echo $first_event_instance['event_id']?>"><?php echo stripslashes_deep($first_event_instance['event_name'])?></h3>
-	
-	<?php if (count($events_group) > 1) :
-			//Show short descriptions
-			if ($first_event_excerpt != '' && $org_options['display_short_description_in_event_list']=='Y'){ ?>
-	   			 <p><?php echo stripslashes_deep(wpautop($first_event_excerpt)); ?></p>
-        <?php }?>
-	    
-	    <script type="text/javascript">
-            $jaer = jQuery.noConflict();
-        </script>
-        
-        <p><a href="#" onClick="$jaer('#date_picker_<?php echo $first_event_instance['event_id']?>').slideDown('slow');$jaer(this).parent().slideUp('slow');return false;" class="register_date" id="register_date_<?php echo $first_event_instance['event_id']?>">Learn More</a></p>
-        <div class="date_picker" id="date_picker_<?php echo $first_event_instance['event_id']?>">
-            <h6 style="margin-top:20px; top:-4px"><?php _e('Available Dates', 'event_espresso'); ?></h6>
-            
-            <ul>
-        	    <?php foreach ($events_group as $e) :
-                    $num_attendees = get_number_of_attendees_reg_limit($e['event_id'], 'num_attendees');//Get the number of attendees. Please visit http://eventespresso.com/forums/?p=247 for available parameters for the get_number_of_attendees_reg_limit() function.
-                    echo '<li>';
-                    if ($num_attendees >= $e['reg_limit']) : 
-                        echo '<span class="error">';
-                    else :
-                        echo '<a href="'.$e['registration_url'].'">';
-                    endif;
-                    if ($e['start_date'] != $e['end_date']) : 
-                        echo event_date_display($e['start_date'], 'l, F j, Y').' – '.event_date_display($e['end_date'], 'l, F j, Y');
-                    else : 
-                        echo event_date_display($e['start_date'], 'l, F j, Y');
-                    endif;
-                    if ($num_attendees >= $e['reg_limit']) : 
-                        echo ' Sold Out</span> <a href="'.get_option('siteurl').'/?page_id='.$e['event_page_id'].'&regevent_action=register&event_id='.$e['overflow_event_id'].'&name_of_event='.stripslashes_deep($e['event_name']).'">'.__('(Join Waiting List)').'</a>';
-                    else :
-                        echo '</a>';
-                    endif;
-                    echo '</li>';
-                endforeach; ?>
-            </ul>
-            
-        </div>
-        
-    <?php else : ?>
-        
-        <h6><?php echo event_date_display($events_group[0]['start_date'], get_option('date_format')); 
-            if ($events_group[0]['start_date'] != $events_group[0]['end_date']) :
-            echo '–'.event_date_display($events_group[0]['end_date'], get_option('date_format'));
-            endif; ?>
-        </h6>
-        
-        <?php //Show short descriptions
-			if ($first_event_excerpt != '' && $org_options['display_short_description_in_event_list']=='Y'){ ?>
-	   			 <p><?php echo stripslashes_deep(wpautop($first_event_excerpt)); ?></p>
-        <?php }?>
-        <?php $num_attendees = get_number_of_attendees_reg_limit($first_event_instance['event_id'], 'num_attendees'); ?>
-        <?php if ($num_attendees >= $events_group[0]['reg_limit']) : ?>
-            
-            <p><span class="error">Sold Out</span> <a href="<?php echo get_option('siteurl')?>/?page_id=<?php echo $first_event_instance['event_page_id']?>&regevent_action=register&event_id=<?php echo $first_event_instance['overflow_event_id']?>&name_of_event=<?php echo stripslashes_deep($first_event_instance['event_name'])?>" title="<?php echo stripslashes_deep($first_event_instance['event_name'])?>"><?php _e('Join Waiting List', 'event_espresso'); ?></a></p> 
-            
-        <?php else : ?>
-            
-            <p><a href="<?php echo $first_event_instance['registration_url']; ?>" title="<?php echo stripslashes_deep($first_event_instance['event_name'])?>"><?php _e('Learn More', 'event_espresso'); ?></a></p>
-        
-        <?php endif; ?>
-    <?php endif; ?>
+<div id="event_data-<?php echo $event_id ?>" class="event_data <?php echo $css_class; ?> <?php echo $category_identifier; ?> event-data-display event-list-display event-display-boxes">
+    <h2 id="event_title-<?php echo $event_id ?>" class="event_title"><a title="<?php echo stripslashes_deep($event_name) ?>" class="a_event_title" id="a_event_title-<?php echo $event_id ?>" href="<?php echo $registration_url; ?>"><?php echo stripslashes_deep($event_name) ?></a>
+        <?php /* These are custom messages that can be displayed based on the event status. Just un-comment the one you want to use. */ ?>
+        <?php //echo $status_display; //Turn this on to display the overall status of the event.  ?>
+        <?php //echo $status_display_ongoing; //Turn this on to display the ongoing message. ?>
+        <?php //echo $status_display_deleted; //Turn this on to display the deleted message. ?>
+        <?php //echo $status_display_secondary; //Turn this on to display the waitlist message. ?>
+        <?php //echo $status_display_reg_closed; //Turn this on to display the registration closed message. ?>
+        <?php //echo $status_display_not_open; //Turn this on to display the secondary message. ?>
+        <?php //echo $status_display_open; //Turn this on to display the not open message. ?>
+        <?php //echo $status_display_custom_closed; //Turn this on to display the closed message. ?>
+    </h2>
+    <?php
+		echo apply_filters('filter_hook_espresso_display_add_to_calendar_by_event_id', $event_id);
+		/* Venue details. Un-comment to display. */ ?>
+    <?php //echo $venue_title != ''?'<p id="event_venue_name-'.$event_id.'" class="event_venue_name">'.stripslashes_deep($venue_title).'</p>':'' ?>
+    <?php //echo $venue_address != ''?'<p id="event_venue_address-'.$event_id.'" class="event_venue_address">'.stripslashes_deep($venue_address).'</p>':''?>
+    <?php //echo $venue_address2 != ''?'<p id="event_venue_address2-'.$event_id.'" class="event_venue_address2">'.stripslashes_deep($venue_address2).'</p>':''?>
+    <?php //echo $venue_city != ''?'<p id="event_venue_city-'.$event_id.'" class="event_venue_city">'.stripslashes_deep($venue_city).'</p>':''?>
+    <?php //echo $venue_state != ''?'<p id="event_venue_state-'.$event_id.'" class="event_venue_state">'.stripslashes_deep($venue_state).'</p>':''?>
+    <?php //echo $venue_zip != ''?'<p id="event_venue_zip-'.$event_id.'" class="event_venue_zip">'.stripslashes_deep($venue_zip).'</p>':''?>
+    <?php //echo $venue_country != ''?'<p id="event_venue_country-'.$event_id.'" class="event_venue_country">'.stripslashes_deep($venue_country).'</p>':''
+    $event->event_cost = empty($event->event_cost) ? '' : $event->event_cost;
+    ?>
 
+    <div class="event-meta">
+			 <p id="p_event_price-<?php echo $event_id ?>" class="event_price"><span class="section-title"><?php  echo __('Price: ', 'event_espresso'); ?></span> <?php echo  $org_options['currency_symbol'].$event->event_cost; ?></p>
+
+        <p id="event_date-<?php echo $event_id ?>"><span class="section-title"><?php _e('Date:', 'event_espresso'); ?></span>  <?php echo event_date_display($start_date, get_option('date_format')) ?></p>
+    </div>
+    <?php
+//Show short descriptions
+    if ($event_desc != '' && isset($org_options['display_short_description_in_event_list']) && $org_options['display_short_description_in_event_list'] == 'Y') {
+        ?>
+        <div class="event-desc">
+            <p><?php echo espresso_format_content($event_desc); ?></p>
+        </div>
+        <?php
+    }
+    ?>
+
+    <?php if ($location != '' && $org_options['display_address_in_event_list'] == 'Y') { ?>
+        <p class="event_address" id="event_address-<?php echo $event_id ?>"><span class="section-title"><?php echo __('Address:', 'event_espresso'); ?></span> <br />
+            <span class="address-block"><?php echo stripslashes_deep($location); ?>
+                <span class="google-map-link"><?php echo $google_map_link; ?></span></span>
+        </p>
+        <?php
+    }
+
+	//Social media buttons
+	do_action('espresso_social_display_buttons', $event_id);
+
+	$num_attendees = get_number_of_attendees_reg_limit($event_id, 'num_attendees'); //Get the number of attendees. Please visit http://eventespresso.com/forums/?p=247 for available parameters for the get_number_of_attendees_reg_limit() function.
+    if ($num_attendees >= $reg_limit) {
+        ?>
+        <p id="available_spaces-<?php echo $event_id ?>"><span class="section-title"><?php _e('Available Spaces:', 'event_espresso') ?> </span><?php echo get_number_of_attendees_reg_limit($event_id, 'available_spaces', 'All Seats Reserved') ?></p>
+        <?php if ($overflow_event_id != '0' && $allow_overflow == 'Y') { ?>
+            <p id="register_link-<?php echo $overflow_event_id ?>" class="register-link-footer"><a class="a_register_link" id="a_register_link-<?php echo $overflow_event_id ?>" href="<?php echo espresso_reg_url($overflow_event_id); ?>" title="<?php echo stripslashes_deep($event_name) ?>"><?php _e('Join Waiting List', 'event_espresso'); ?></a></p>
+            <?php
+        }
+    } else {
+        if ($display_reg_form == 'Y' && $externalURL == '') {
+            ?>			<p id="available_spaces-<?php echo $event_id ?>" class="spaces-available"><span class="section-title"><?php _e('Available Spaces:', 'event_espresso') ?></span> <?php echo get_number_of_attendees_reg_limit($event_id, 'available_spaces') ?></p>
+            <?php
+        }
+
+        /**
+         * Load the multi event link.
+         * */
+        //Un-comment these next lines to check if the event is active
+        //echo event_espresso_get_status($event_id);
+        //print_r( event_espresso_get_is_active($event_id));
+
+        if ($multi_reg && event_espresso_get_status($event_id) == 'ACTIVE') {
+
+            $params = array(
+                //REQUIRED, the id of the event that needs to be added to the cart
+                'event_id' => $event_id,
+                //REQUIRED, Anchor of the link, can use text or image
+                'anchor' => __("Add to Cart", 'event_espresso'), //'anchor' => '<img src="' . EVENT_ESPRESSO_PLUGINFULLURL . 'images/cart_add.png" />',
+                //REQUIRED, if not available at this point, use the next line before this array declaration
+                // $event_name = get_event_field('event_name', EVENTS_DETAIL_TABLE, ' WHERE id = ' . $event_id);
+                'event_name' => $event_name,
+                //OPTIONAL, will place this term before the link
+                'separator' => __(" or ", 'event_espresso')
+            );
+
+            $cart_link = event_espresso_cart_link($params);
+        }
+        if ($display_reg_form == 'Y') {
+            ?>
+
+            <p id="register_link-<?php echo $event_id ?>" class="register-link-footer">
+                <a class="a_register_link" id="a_register_link-<?php echo $event_id ?>" href="<?php echo $registration_url; ?>" title="<?php echo stripslashes_deep($event_name) ?>"><?php _e('Register', 'event_espresso'); ?></a>
+                <?php echo isset($cart_link) && $externalURL == '' ? $cart_link : ''; ?>
+            </p>
+        <?php } else { ?>
+            <p id="register_link-<?php echo $event_id ?>" class="register-link-footer">
+                <a class="a_register_link" id="a_register_link-<?php echo $event_id ?>" href="<?php echo $registration_url; ?>" title="<?php echo stripslashes_deep($event_name) ?>"><?php _e('View Details', 'event_espresso'); ?></a> <?php echo isset($cart_link) && $externalURL == '' ? $cart_link : ''; ?>
+            </p>
+        <?php
+        }
+    }
+    ?>
 </div>
