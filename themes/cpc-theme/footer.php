dc91a44d0e274e39d05b70d2b87c9ff3083b86b6
Matt Stratton <matt.stratton@gmail.com>
2012-03-21 08:34:20 -0500

    Removal of ccp2012 from footer.php

--- themes/cpc-theme/footer.php
+++ themes/cpc-theme/footer.php
@@ -6,6 +6,8 @@
 </div>	<!--site closing tag-->
 
 <?php wp_footer(); ?>
+<!--
 <div class="float_banner"><a href="http:// www.chicagoculturalplan2012.com" target="_new"><img src= "http://www.goweetu.com/ccp/bug/supportccp2012.png" alt="We Support Chicago Cultural Plan 2012" height="125" width="125" /></a></div>
+-->
 </body>
 </html>
