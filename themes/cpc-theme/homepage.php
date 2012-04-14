df8ebfe68c2127691216ce08633d33025c042e03
Matt Stratton <matt.stratton@gmail.com>
2012-04-13 23:31:22 -0500

    added another br before learn more

--- themes/cpc-theme/homepage.php
+++ themes/cpc-theme/homepage.php
@@ -76,7 +76,7 @@ Template Name: Home Page
 							while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
 							<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
 							<h2><?php the_title(); ?></h2>
-							  <?php the_excerpt(); ?><BR/>
+							  <?php the_excerpt(); ?><BR/><BR/>
 							  <a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>">>>LEARN MORE</a>							  <br />							  <hr style="height: 2px;"/>
 						  </div>
 						<?php endwhile; ?>
